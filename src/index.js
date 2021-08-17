90dc29731d224f57a25ed736421c7c9bf0d433f6
Lucas Craveiro Paes <lucascraveiropaes@gmail.com>
2021-08-17 17:05:17 -0300

    Atualizando index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
-const http = require('http');
-const JSONServer = require('@discovery-solutions/json-server');
+const JSONServer = require("@discovery-solutions/json-server").default;
 
-const port = 3000;
-const host = '127.0.0.1';
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello world');
-});
-
-const servers = new JSONServer.startServers({
-  database: {
-    url: "mongodb+srv://adm:ademir@cluster0.melvy.mongodb.net/Cluster0?retryWrites=true&w=majority",
-    name: "Cluster0",
-    type: "mongo"
-  }
-});
-
-server.listen(port, host, () => {
-  console.log(`O servidor está rodando em http://${host}:${port}`);
+const servers = new JSONServer({
+	project: "User Example",
+	token: "6)e(0grtKiJm)hfYJ0CK)833rcmcHJqpfWrQtjb7p7)MZcPD9BLI92(GfjnG3TS(EzhXrV)H@xbB*MgH4@X6dhRIqCzq5(hMdfB*zGze1TE4v0FMvCODpVxjIBl2B3(AqQmcpAryYU8WLKZ22u$!UyXy0ZXpQMdVOojvrM@I9zRuG6T@4CmLnoHaZSIN$l*s58)nF77$c55MIn$VSZbnUXpxbG!P416oKOPqULmTGROHxsqUuNKuyhbk69D8kX(5",
+	servers: {
+	    rest: 4500,
+	    socket: 5000,
+	    file: 4750,
+	},
+	database: {
+		url: "mongodb+srv://adm:ademir@cluster0.melvy.mongodb.net/Cluster0?retryWrites=true&w=majority",
+		name: "Cluster0",
+		type: "mongo"
+	},
+	entities: [{
+        name: "user",
+        fields: [{
+            name: "name",
+            type: "string",
+        }, {
+            name: "email",
+            type: "string"
+        }, {
+            name: "birthdate",
+            type: "date"
+        }]
+    }]
 });
